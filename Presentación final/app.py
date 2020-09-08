f3a4c675dd2cb232966ebbe0d73d2d5269181acf
yoshimar <ycmunoz90@gmail.com>
2020-09-08 08:39:18 -0500

    final

--- Presentación final/app.py
+++ Presentación final/app.py
@@ -1,28 +1,80 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
+import formsFunc as ff
+import bioFunc as bf
+import os
 
-app = Flask(__name__) 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_key'
 bootstrap = Bootstrap(app)
 fa = FontAwesome(app)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html",title="INICIO")
+    return render_template("index.html", title="INICIO")
 
-@app.route("/Analisis/Proteina")
+
+@app.route("/Analisis", methods=('GET', 'POST'))
+@app.route("/Analisis/Gen", methods=('GET', 'POST'))
+def gen():
+    form_g = ff.gen_form()
+    lista = bf.importar_genes("Chikunguya")
+    lista_homologos = []
+    form_g.gen_list.choices = lista
+    if form_g.validate_on_submit():
+        flash('Gen escogido = %s' % (form_g.gen_list.data))
+        indice = int(form_g.gen_list.data)
+        file_homologo = form_g.gen_list.data
+        file_homologo += '.fasta'
+        lista_homologos = bf.importar_homologos_G(file_homologo)
+        info = bf.getSeq_G("Chikunguya", lista[indice-1][1])
+        return render_template('analisis_gen.html', title='Gen', form=form_g, lista=lista_homologos, info=info)
+
+    return render_template("analisis_gen.html", title="ANÁLISIS GENES", form=form_g)
+
+
+@app.route("/Analisis/Proteina", methods=('GET', 'POST'))
 def proteina():
-    return render_template("analisis_prot.html",title="ANALISIS PROTEINAS")
+    form_p = ff.protein_form()
+    lista = bf.importar_proteinas("Chikunguya")
+    lista_homologos = []
+    form_p.protein_list.choices = lista
+    if form_p.validate_on_submit():
+        flash('Proteina escogida = %s' % (form_p.protein_list.data))
+        indice = int(form_p.protein_list.data)
+        file_homologo = form_p.protein_list.data
+        file_homologo += '.fasta'
+        lista_homologos = bf.importar_homologos_P(file_homologo)
+        info = bf.getSeq_P("Chikunguya", lista[indice-1][1])
+        return render_template('analisis_prot.html', title='Proteina', form=form_p, lista=lista_homologos, info=info)
+    return render_template("analisis_prot.html", title="ANALISIS PROTEINAS", form=form_p)
 
-#manejando rutas con variables dadas
-@app.route("/Analisis/Arbol")
+# manejando rutas con variables dadas
+
+
+@app.route("/Analisis/Arbol", methods=('GET', 'POST'))
 def arbol():
-    return render_template("analisis_arbol.html",title="ANÁLISIS ARBOL FILOGENÉTICO")
+    form_t = ff.tree_form()
+    lista = bf.importar_genes("Chikunguya")
+    form_t.tree_list.choices = lista
+    if form_t.validate_on_submit():
+        flash('ID: %s' % (form_t.tree_list.data))
+        indice = form_t.tree_list.data
+        path_arbol_num = './static/docs/Homologos/Nucleotidos'+form_t.tree_list.data
+        comando = 'clustalw ' + path_arbol_num + '.fasta'
+        os.system(comando)
+        bf.generar_arbol(path_arbol_num + '.aln', indice)
+        info = list(open('./static/docs/Informacion/'+indice+'.txt', 'r'))[1]
+        return render_template('analisis_arbol.html', title='Arbol', form=form_t, indice=indice, info=info)
+    return render_template("analisis_arbol.html", title="ANÁLISIS ARBOL FILOGENÉTICO", form=form_t)
+
 
 @app.route("/About")
 def about():
-    return render_template("about.html",title="ACERCA DE")
+    return render_template("about.html", title="ACERCA DE")
 
 
 if __name__ == "__main__":
-    app.run(debug=False,port=3000)   
+    app.run(debug=False, port=3000)
---
+++ Presentación final/bioFunc.py
@@ -0,0 +1,97 @@
+# for BioPython
+import pylab
+from Bio import Phylo
+from Bio.Blast import NCBIWWW
+from Bio.Seq import Seq
+from Bio import SeqIO
+from Bio.Alphabet import generic_dna, generic_protein, generic_rna
+from Bio import Entrez
+from Bio import SeqIO
+# for tree
+from Bio import AlignIO
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+import matplotlib
+matplotlib.use('Agg')  # para resolver lo de la imagen
+
+
+def getSeq_P(especie, acc_id):
+    rec = SeqIO.parse("./static/docs/Proteinas/"+especie+".fasta", "fasta")
+    for seq in rec:
+        if seq.id == acc_id:
+            seqID = '>' + str(seq.description)
+            seqSeq = str(seq.seq)
+            break
+    data = {'ID': seqID, 'Seq': seqSeq}
+    return data
+
+
+def getSeq_G(especie, acc_id):
+    rec = SeqIO.parse("./static/docs/Genes/"+especie+".fasta", "fasta")
+    for seq in rec:
+        if seq.id == acc_id:
+            seqID = '>' + str(seq.description)
+            seqSeq = str(seq.seq)
+            break
+    data = {'ID': seqID, 'Seq': seqSeq}
+    return data
+
+
+def importar_genes(especie):
+    rec = SeqIO.parse("./static/docs/Genes/"+especie+".fasta", "fasta")
+    i = 1
+    out_key = []
+    out_id = []
+    for r in rec:
+        out_key.append(str(i))
+        out_id.append(r.id)
+        i += 1
+    return list(zip(out_key, out_id))
+
+
+def importar_proteinas(especie):
+    rec = SeqIO.parse("./static/docs/Proteinas/"+especie+".fasta", "fasta")
+    i = 1
+    out_key = []
+    out_id = []
+    for r in rec:
+        out_key.append(str(i))
+        out_id.append(r.id)
+        i += 1
+    return list(zip(out_key, out_id))
+
+
+def importar_homologos_G(file):
+    file = "./static/docs/Homologos/Nucleotidos" + file
+    rec = SeqIO.parse(file, "fasta")
+    out_accession = []
+    out_description = []
+    for r in rec:
+        out_accession.append(r.id)
+        out_description.append(r.description)
+    return list(zip(out_accession, out_description))
+
+
+def importar_homologos_P(file):
+    file = "./static/docs/Homologos/Proteinas" + file
+    rec = SeqIO.parse(file, "fasta")
+    out_accession = []
+    out_description = []
+    for r in rec:
+        out_accession.append(r.id)
+        out_description.append(r.description)
+    return list(zip(out_accession, out_description))
+
+
+def generar_arbol(file, indice):
+    with open(file, "r") as aln:
+        alineamiento = AlignIO.read(aln, "clustal")
+
+    calculator = DistanceCalculator('identity')
+    dm = calculator.get_distance(alineamiento)
+
+    constructor = DistanceTreeConstructor(calculator)
+    nj = constructor.nj(dm)  # Neighbor Joining
+    Phylo.draw(nj)
+    path = './static/assets/arbol_filogenetico' + indice + '.png'
+    pylab.savefig(path, format='png')
